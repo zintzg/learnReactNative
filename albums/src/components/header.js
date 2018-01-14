a8d4ba709d3cfa70cdd0281141ffb1fce68c0e63
zintzg <zintz.g@gmail.com>
2018-01-14 10:06:40 -0500

    learnReactNative Continued

---
+++ albums/src/components/header.js
@@ -0,0 +1,11 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create a component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//Make component available to other parts of the app
+export default Header;
