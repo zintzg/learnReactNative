ae63b36f10778f2b6fccf0e0d8af20625f01ce7d
zintzg <zintz.g@gmail.com>
2018-01-14 12:46:42 -0500

    learnReact Day 2

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,15 @@
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Text>Some Text</Text>
+  <View>
+  <Header headerText={'Albums'} />
+  <AlbumList />
+  </View>
 );
 
 //Render it to the device
