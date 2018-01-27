dd9b81f0c96bb3b07c4410c13737a19a8ee999ab
zintzg <zintz.g@gmail.com>
2018-01-27 12:57:31 -0500

    Completed Alums App!

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -27,11 +28,18 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
         style={imageStyle}
         source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+        Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
@@ -57,7 +65,7 @@ const AlbumDetail = ({ album }) => {
       marginLeft: 10,
       marginRight: 10,
     },
-    
+
     imageStyle: {
       height: 300,
       flex: 1,
