412cf0a4bbe425321f327c1cd9ffe78b6a8d9db4
zintzg <zintz.g@gmail.com>
2018-02-24 17:18:52 -0500

    learnReactNative 2-24

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render () {
     return (
       <View>
-        <Text>An App</Text>
+        <Header headerText="Authentication"/>
       </View>
     );
   }
