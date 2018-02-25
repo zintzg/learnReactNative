145a6af13d7438c95f8f73ad6238358b5f11208e
zintzg <zintz.g@gmail.com>
2018-02-25 14:40:36 -0500

    learnReactNative - auth 2/25

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount () {
+    firebase.initializeApp ({
+    apiKey: 'AIzaSyAsQIT9UJGxO2RssSUJ1r8JJJBBSsFcPVI',
+    authDomain: 'auth-71123.firebaseapp.com',
+    databaseURL: 'https://auth-71123.firebaseio.com',
+    projectId: 'auth-71123',
+    storageBucket: 'auth-71123.appspot.com',
+    messagingSenderId: '384673546250'
+  });
+  }
   render () {
     return (
       <View>
         <Header headerText="Authentication"/>
+        <LoginForm />
       </View>
     );
   }
---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+  state = { email: '', password: '', error:'' };
+
+  onButtonPress() {
+    const = { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.atuh().createUserWithEmailAndPassword(email, password)
+        .catch (() => {
+          this.setState({ error: 'Authentication Failed!' });
+        });
+      });
+  }
+
+  render () {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+          placeholder="user@gmail.com"
+          label="Email"
+           value={this.state.email}
+           onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+        <Input
+        secureTextEntry={true}
+        placeholder="password"
+        label="Password"
+         value={this.state.password}
+         onChangeText={password => this.setState({ password })}
+        />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+export default LoginForm;
